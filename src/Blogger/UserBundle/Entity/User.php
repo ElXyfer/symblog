a989e5f419475899299689ab28898be097908678
Lincoln.chawora <Lincoln.chawora@digital.hmrc.gov.uk>
2017-11-10 20:45:30 +0000

    hopefully not broken

--- src/Blogger/UserBundle/Entity/User.php
+++ src/Blogger/UserBundle/Entity/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Blogger\BlogBundle\Entity;
+namespace UserBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use FOS\UserBundle\Model\User as BaseUser;
@@ -9,13 +9,13 @@ use FOS\UserBundle\Model\User as BaseUser;
  * User
  *
  * @ORM\Table(name="ws_user")
- * @ORM\Entity(repositoryClass="Blogger\BlogBundle\Repository\UserRepository")
+ * @ORM\Entity(repositoryClass="Blogger\UserBundle\Repository\UserRepository")
  */
 class User extends  BaseUser
 {
 
     /**
-     * @ORM\OneToMany(targetEntity="Post", mappedBy="user")
+     * @ORM\OneToMany(targetEntity="Blogger\BlogBundle\Entity\Post", mappedBy="user")
      */
     private $posts;
 
