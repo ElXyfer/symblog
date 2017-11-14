bdc6bea8b6cdf4fdbc583d9de2bcf9df97990656
Lincoln.chawora <Lincoln.chawora@digital.hmrc.gov.uk>
2017-11-14 18:12:30 +0000

    deleted false routes

--- src/Blogger/UserBundle/Entity/User.php
+++ src/Blogger/UserBundle/Entity/User.php
@@ -20,6 +20,12 @@ class User extends  BaseUser
     private $posts;
 
     /**
+     * @var \Doctrine\Common\Collections\ArrayCollection
+     * @ORM\OneToMany(targetEntity="Blogger\BookBundle\Entity\Book", mappedBy="submitted_by")
+     */
+    private $books;
+
+    /**
      * @var int
      *
      * @ORM\Column(name="id", type="integer")
@@ -31,6 +37,75 @@ class User extends  BaseUser
     public function __construct()
     {
         parent::__construct();
+        $this->books = new \Doctrine\Common\Collections\ArrayCollection();
     }
 
+
+    /**
+     * Add post
+     *
+     * @param \Blogger\BlogBundle\Entity\Post $post
+     *
+     * @return User
+     */
+    public function addPost(\Blogger\BlogBundle\Entity\Post $post)
+    {
+        $this->posts[] = $post;
+
+        return $this;
+    }
+
+    /**
+     * Remove post
+     *
+     * @param \Blogger\BlogBundle\Entity\Post $post
+     */
+    public function removePost(\Blogger\BlogBundle\Entity\Post $post)
+    {
+        $this->posts->removeElement($post);
+    }
+
+    /**
+     * Get posts
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getPosts()
+    {
+        return $this->posts;
+    }
+
+    /**
+     * Add book
+     *
+     * @param \Blogger\BookBundle\Entity\Book $book
+     *
+     * @return User
+     */
+    public function addBook(\Blogger\BookBundle\Entity\Book $book)
+    {
+        $this->books[] = $book;
+
+        return $this;
+    }
+
+    /**
+     * Remove book
+     *
+     * @param \Blogger\BookBundle\Entity\Book $book
+     */
+    public function removeBook(\Blogger\BookBundle\Entity\Book $book)
+    {
+        $this->books->removeElement($book);
+    }
+
+    /**
+     * Get books
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getBooks()
+    {
+        return $this->books;
+    }
 }
