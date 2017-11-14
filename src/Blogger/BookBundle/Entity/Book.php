acd9cd1b0e1e633348324a62e315e628d442f8b0
Lincoln.chawora <Lincoln.chawora@digital.hmrc.gov.uk>
2017-11-14 17:43:20 +0000

    Working book bundle and entity

---
+++ src/Blogger/BookBundle/Entity/Book.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace Blogger\BookBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Book
+ *
+ * @ORM\Table(name="books")
+ * @ORM\Entity(repositoryClass="Blogger\BookBundle\Repository\BookRepository")
+ */
+class Book
+{
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="book_author", type="string", length=255)
+     */
+    private $bookAuthor;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="picture", type="string", length=255, nullable=true)
+     */
+    private $picture;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="year_published", type="date", nullable=true)
+     */
+    private $yearPublished;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Book
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set bookAuthor
+     *
+     * @param string $bookAuthor
+     *
+     * @return Book
+     */
+    public function setBookAuthor($bookAuthor)
+    {
+        $this->bookAuthor = $bookAuthor;
+
+        return $this;
+    }
+
+    /**
+     * Get bookAuthor
+     *
+     * @return string
+     */
+    public function getBookAuthor()
+    {
+        return $this->bookAuthor;
+    }
+
+    /**
+     * Set picture
+     *
+     * @param string $picture
+     *
+     * @return Book
+     */
+    public function setPicture($picture)
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    /**
+     * Get picture
+     *
+     * @return string
+     */
+    public function getPicture()
+    {
+        return $this->picture;
+    }
+
+    /**
+     * Set yearPublished
+     *
+     * @param \DateTime $yearPublished
+     *
+     * @return Book
+     */
+    public function setYearPublished($yearPublished)
+    {
+        $this->yearPublished = $yearPublished;
+
+        return $this;
+    }
+
+    /**
+     * Get yearPublished
+     *
+     * @return \DateTime
+     */
+    public function getYearPublished()
+    {
+        return $this->yearPublished;
+    }
+}
+
