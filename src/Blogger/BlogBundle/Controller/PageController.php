2ce889c57d9308048ae19f2917692d9707fb0c05
Lincoln.chawora <Lincoln.chawora@digital.hmrc.gov.uk>
2017-11-15 13:37:01 +0000

    removed user and book bundles, added user is logged in filter post function

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -7,11 +7,20 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class PageController extends Controller
 {
+    public function getCurrentUserID() {
+        if ($this->getUser())
+        {
+            return $this->getUser()->getId();
+        } else {
+            return null;
+        }
+    }
+
     public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
         $blogPost = $em->getRepository('BloggerBlogBundle:Post')
-                ->getLatest(10, 0);
+                ->getLatest(10, 0, $this->getCurrentUserID());
         return $this->render('BloggerBlogBundle:Page:index.html.twig',
             ['blogposts' => $blogPost]);
 
--- src/Blogger/BookBundle/BookBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Blogger\BookBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class BookBundle extends Bundle
-{
-}
